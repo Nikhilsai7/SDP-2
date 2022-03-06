44b05e78fd5166b49fecf83dc8ac825acc67d7a0
Nikhil Sai <legendnikhil22@gmail.com>
2022-03-06 12:13:35 +0530

    first commit

--- ec3/settings.py
+++ ec3/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='venkateshvenky5408@gmail.com'
-EMAIL_HOST_PASSWORD='Venky@150102#'
+EMAIL_HOST_USER='noreplay.edeals@gmail.com'
+EMAIL_HOST_PASSWORD='E-DealsWeb'
 
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
